ffc711b2ae4f172fab94a399abd46c6599aa6387
Niccolò <niccolo.sorrenti@gmail.com>
2026-06-02 15:25:37 +0200

    commit

---
+++ basketballStatsTrackerBackend/src/main/java/niccolosorrenti/basketballStatsTrackerBackend/BasketballStatsTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package niccolosorrenti.basketballStatsTrackerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballStatsTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballStatsTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ basketballStatsTrackerBackend/src/test/java/niccolosorrenti/basketballStatsTrackerBackend/BasketballStatsTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package niccolosorrenti.basketballStatsTrackerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballStatsTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
